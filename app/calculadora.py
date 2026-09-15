154a93cccbd12b517e6e5d8a8a19fc909b07a2c7
R-Lkz <melorenan695@gmail.com>
2026-09-15 15:02:13 -0300

    Corrige multiplicacao

--- app/calculadora.py
+++ app/calculadora.py
@@ -3,7 +3,7 @@ def somar(a,b):
 def subtrair(a,b):
     return a - b
 def multiplicar(a,b):
-    return a + b
+    return a * b
 def dividir(a,b):
     if b == 0:
         raise ValueError("Não é possível dividir porrr zero.")
