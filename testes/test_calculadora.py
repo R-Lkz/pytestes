05df6f9cc9fac6e55f87c4c56a2b308b5850a2a2
R-Lkz <melorenan695@gmail.com>
2026-09-15 14:58:59 -0300

    Adiciona multiplicacao

--- testes/test_calculadora.py
+++ testes/test_calculadora.py
@@ -1,7 +1,8 @@
 from app.calculadora import (
     somar,
     subtrair,
-    calcular_desconto
+    calcular_desconto,
+    multiplicar
 )
 
 def test_somar():
@@ -15,3 +16,7 @@ def test_subtrair():
 def test_calcular_desconto():
     resultado = calcular_desconto(100,10)
     assert resultado == 90
+
+def test_multiplicar():
+    resultado = multiplicar(4,5)
+    assert resultado == 20
